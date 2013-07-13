4184f5b754dc36867132ecc975906f8526d25510
rsdk <rnkblr@gmail.com>
2013-07-13 13:00:12 +0200

    xml parsen um title und link zu bekommen

--- fefe.py
+++ fefe.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as ET
 
 req = urllib.request.urlopen('http://blog.fefe.de/rss.xml')
 #print(req.read().decode('utf-8'))
-tree = ET.parse(req.read().decode('utf-8'))
+tree = ET.parse(req)
 root = tree.getroot()
-for child in root:
-    print(child.tag, child.attrib)
+for child in root[0].findall('item'):
+    print(child[0].text, child[1].text)
