0fed0500ce84b5530c753c1c3128719a71057271
michaelmdk1 <michaelmdkuebler@gmail.com>
2013-07-13 12:44:55 +0200

    xml-parsen

--- fefe.py
+++ fefe.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 import urllib.request
+import xml.etree.ElementTree as ET
+
 req = urllib.request.urlopen('http://blog.fefe.de/rss.xml')
-print(req.read().decode('utf-8'))
+#print(req.read().decode('utf-8'))
+tree = ET.parse(req.read().decode('utf-8'))
+root = tree.getroot()
+for child in root:
+    print(child.tag, child.attrib)
