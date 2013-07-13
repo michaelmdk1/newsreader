25adb75535ac26d3c23a0446dbdccf980acdc9ad
michaelmdk1 <michaelmdkuebler@gmail.com>
2013-07-13 14:16:16 +0200

    einfügen nur von neuen Elementen in die Datenbank

--- fefe.py
+++ fefe.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 import urllib.request
 import xml.etree.ElementTree as ET
+import sqlite3
+import datetime
 
 req = urllib.request.urlopen('http://blog.fefe.de/rss.xml')
 #print(req.read().decode('utf-8'))
 tree = ET.parse(req)
 root = tree.getroot()
+conn = sqlite3.connect('news.db')
+c = conn.cursor()
+
 for child in root[0].findall('item'):
-    print(child[0].text, child[1].text)
+    #print(child[0].text, child[1].text)
+
+    if not c.execute("select uid from fefe where uid=:id", {"id": child[1].text}).fetchone():
+        c.execute('insert into fefe values (?,?,?)', (child[1].text, child[0].text, datetime.datetime.today()))
+conn.commit()
+c.close()
+
